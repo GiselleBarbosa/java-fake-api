b4b4edf99d2243bf7545dc63f6f03f4a6cdb0c86
GiselleBarbosa <g88barbosa@gmail.com>
2024-10-06 19:03:44 -0300

    fix: corrige parametro do feignClient

--- src/main/java/com/barbosa/fakeapi/infrastructure/FakeApiClient.java
+++ src/main/java/com/barbosa/fakeapi/infrastructure/FakeApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(value = "fake-api", url = "${fake-api-url:#{null}")
+@FeignClient(value = "fake-api", url = "${fake-api.url:#{null}}")
 public interface FakeApiClient {
 
     @GetMapping("/products")
