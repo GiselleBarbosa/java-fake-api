30830a5d1dc7024276026966e5c0f16ba6fab343
GiselleBarbosa <g88barbosa@gmail.com>
2024-10-06 18:50:53 -0300

    feat: implementa controller e habilita documentacao com SpringDoc OpenAPI Starter WebMVC UI

---
+++ src/main/java/com/barbosa/fakeapi/apiv1/dto/FakeApiController.java
@@ -0,0 +1,34 @@
+package com.barbosa.fakeapi.apiv1.dto;
+
+import com.barbosa.fakeapi.business.FakeApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+@RequiredArgsConstructor
+@Tag(name = "fake-api")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos produtos")
+})
+public class FakeApiController {
+
+    private final FakeApiService fakeApiService;
+
+    @Operation(summary = "Busca todos os produtos", method = "GET")
+    @GetMapping("")
+    public ResponseEntity<List<ProductsDto>> buscaTodosProdutos() {
+        return ResponseEntity.ok(fakeApiService.buscaTodosProdutos());
+    }
+
+}
