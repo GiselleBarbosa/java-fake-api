cd90518b3eceea2eb07a793edc96d2ab1aa9d47d
GiselleBarbosa <g88barbosa@gmail.com>
2024-10-06 18:22:19 -0300

    feat: implementa configuracoes do client

--- src/main/java/com/barbosa/fakeapi/infrastructure/FakeApiClient.java
+++ src/main/java/com/barbosa/fakeapi/infrastructure/FakeApiClient.java
@@ -1,7 +1,14 @@
 package com.barbosa.fakeapi.infrastructure;
 
+import com.barbosa.fakeapi.apiv1.dto.ProductsDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(value = "fake-api", url = "${fake-api-url:#{null}")
-public class FakeApiClient {
+public interface FakeApiClient {
+
+    @GetMapping("/products")
+    List<ProductsDto> buscaTodosProdutos();
 }
